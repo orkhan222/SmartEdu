10085bcb6b03623b2b9bf34aa714b574c61150fb
Salman Mustafayev <salmanmustafayev03@gmail.com>
2021-08-22 11:57:58 +0400

    Login register yazildi

--- lahiye_con/lahiye/settings.py
+++ lahiye_con/lahiye/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'pages',
     'courses',
     'teachers',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- lahiye_con/lahiye/urls.py
+++ lahiye_con/lahiye/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('',include('pages.urls')),
     path('courses/',include('courses.urls')),
     path('teachers/',include('teachers.urls')),
+    path('accounts/',include('accounts.urls')),
     
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
